a1a849dc64b1d7f831b3e545443c9e35d4018e74
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-07-29 14:42:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const holes = document.querySelectorAll(".hole")
+const moles = document.querySelectorAll(".mole")
+const scoreBoard = document.querySelectorAll(".score")
+const startBtn = document.querySelectorAll(".start-btn")
+const levels  = document.querySelectorAll(".levels")
+const game  = document.querySelectorAll(".game")
+
+let lastHole;
+let timeUp =  false;
+let score=0;
+
+function difficultyLevel(){
+    const ele = document.getElementsByName("level");
+    for(let )
+}
